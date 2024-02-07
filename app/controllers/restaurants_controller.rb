ad61f3e96d540b06719364ac927f610f98893f3f
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2024-02-07 21:19:11 +0000

    finish main functionality of the website

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,26 +6,32 @@ class RestaurantsController < ApplicationController
   end
 
   def show
-
   end
 
   def new
-
+    @restaurant = Restaurant.new
   end
 
   def create
-
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
-
   end
 
   def update
-
+    @restaurant.update(restaurant_params)
+    redirect_to restaurants_path
   end
 
   def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path
 
   end
 
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,14 @@
 class Restaurant < ApplicationRecord
-  has_many :interviews, dependent: :destroy
+  has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :address, presence: true
-  validates :category, presence: true, inclusion: { in: %w(chinese italian japanese french belgian) }
+  validates :category, presence: true, inclusion: { in: [ "chinese", "italian", "japanese", "french", "belgian" ] }
+
+  before_validation :normalize_category
+
+  private
+
+  def normalize_category
+    self.category = category.downcase.strip if category.present?
+  end
 end
